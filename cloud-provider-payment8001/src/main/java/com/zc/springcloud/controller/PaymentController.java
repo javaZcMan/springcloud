c3a29a18f332ef66de7af3b78a7940ede784af1f
userzc <userzc@qq.com>
2022-11-06 23:36:34 +0800

    修改

--- cloud-provider-payment8001/src/main/java/com/zc/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/zc/springcloud/controller/PaymentController.java
@@ -20,7 +20,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("****插入结果*****:" + result);
         if (result > 0) {
@@ -31,7 +31,8 @@ public class PaymentController {
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        
         Payment payment = paymentService.getPaymentById(id);
         log.info("****查询结果*****:" + payment);
         if (payment != null) {
